8d32f9f9d210795121f5600a84f9d2f534ab73a9
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-02-14 20:40:03 +0500

    gino db/declare models

---
+++ tgbot/models/models.py
@@ -0,0 +1,29 @@
+from gino import Gino
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+db = Gino()
+
+
+class Users(db.Model):
+
+    __tablename__ = 'users'
+
+    tg_id = Column(Integer(), primary_key=True)
+
+
+class Room(db.Model):
+
+    __tablename__ = 'room'
+
+    Id = Column(Integer(), primary_key=True)
+    name = Column(String(120))
+    owner = Column(ForeignKey("users.tg_id"))
+
+
+class GameMembers(db.Model):
+
+    __tablename__ = 'game_members'
+
+    Id = Column(Integer(), primary_key=True)
+    user_id = Column(ForeignKey("users.tg_id"))
+    room_id = Column(ForeignKey("room.Id"))
